2551d1b0b2c9e01768609a0e5528b0df2c213fab
Hardik Naik <96820742+mehardiknaik@users.noreply.github.com>
2026-02-14 18:57:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default ({ mode }: any) => {
-  // Load environment variables based on the current mode
-  const env = loadEnv(mode, process.cwd(), '');
+export default () => {
 
   return defineConfig({
     plugins: [react()],
-    base: env.VITE_PUBLIC_PATH || '/', // Use the env variable, or default to '/'
+    base: '/fun-v/', // Use the env variable, or default to '/'
     // other config options
   });
-};
+}
